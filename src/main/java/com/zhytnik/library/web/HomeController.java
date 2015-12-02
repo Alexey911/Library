a47b57a5f01e3404a843306f3e3cd19efa887578
AlexeyZhytnik <alexeyzhytnik@gmai.com>
2015-12-02 16:49:25 +0400

    fix bug access to resources

--- src/main/java/com/zhytnik/library/web/HomeController.java
+++ src/main/java/com/zhytnik/library/web/HomeController.java
@@ -1,12 +1,16 @@
 package com.zhytnik.library.web;
 
+import com.zhytnik.library.security.MinAccessed;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import static com.zhytnik.library.security.UserRole.USER;
+
 @Controller
 public class HomeController {
+    @MinAccessed(USER)
     @RequestMapping(value = {"/", "/home", "/index"}, method = RequestMethod.GET)
     public ModelAndView showHomePage() {
         return new ModelAndView("index", "info", "Hello User :)");
